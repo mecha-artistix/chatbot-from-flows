66b720e78df8967a7a5291471807a73ad282713d
mecha-artistix <mecha.artistix@gmail.com>
2024-09-09 16:30:25 +0500

    leads added

--- cb_app_backend/src/models/leadModel.ts
+++ cb_app_backend/src/models/leadModel.ts
@@ -6,7 +6,8 @@ const leadSchema = new Schema<ILead>({
   //   user: { type: Schema.ObjectId, ref: 'User', required: true },
   //   name: { type: String, required: false },
   createdAt: { type: Date, default: Date.now },
-  intent: { sessionId: { type: String }, intent: { type: String } },
+  sessionId: { type: String },
+  intent: { type: String },
 });
 
 const Lead = mongoose.model('Lead', leadSchema);
