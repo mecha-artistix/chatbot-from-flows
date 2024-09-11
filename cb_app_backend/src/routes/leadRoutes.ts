19a4520a69bd5c2a2e6783df1cbe7312ad530318
mecha-artistix <mecha.artistix@gmail.com>
2024-09-11 17:42:58 +0500

    leads updated

--- cb_app_backend/src/routes/leadRoutes.ts
+++ cb_app_backend/src/routes/leadRoutes.ts
@@ -1,11 +1,17 @@
 import express, { Router } from 'express';
-import { createLead, getAllLeads, deleteLead, updateLead, getPaginatedLeads } from '../controllers/leadController';
+import {
+  createLead,
+  getAllLeads,
+  deleteLead,
+  updateLead,
+  getPaginatedLeads,
+  leadsStats,
+} from '../controllers/leadController';
 
 const router: Router = express.Router();
 
-// router.route('/').post(createLead).get(getAllLeads);
-router.route('/').post(createLead).get(getPaginatedLeads);
-
+router.route('/').post(createLead).get(getAllLeads);
+router.route('/stats').get(leadsStats);
 router.route('/:id').delete(deleteLead).patch(updateLead);
 
 export default router;
