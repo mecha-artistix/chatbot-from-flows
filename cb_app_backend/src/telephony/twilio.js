4577d62b86b826459027990d1b46cb5a2734969c
mecha-artistix <mecha.artistix@gmail.com>
2024-09-25 10:03:00 +0500

    starting to work on twilio after reverting commits and removing twilio secrets

--- cb_app_backend/src/telephony/twilio.js
+++ cb_app_backend/src/telephony/twilio.js
@@ -1,12 +1,11 @@
-const twilio = require('twilio');
 const dotenv = require('dotenv');
+const twilio = require('twilio');
 
 dotenv.config({ path: './../../.env' });
 
 const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN } = process.env;
 
 const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
-
 
 async function createCall() {
   const call = await client.calls.create({
